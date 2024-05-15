5ed17a4cccef2a8397694564958d7e0892fd7f77
sahumajurado <2113110059@untels.edu.pe>
2024-05-15 10:54:48 -0500

    Parte 03 - Listo

--- DAM_GERSONSAHUMA/app/src/main/java/com/example/dam_gersonsahuma/MainActivity.java
+++ DAM_GERSONSAHUMA/app/src/main/java/com/example/dam_gersonsahuma/MainActivity.java
@@ -3,13 +3,16 @@ package com.example.dam_gersonsahuma;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+import android.widget.Button;
+import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn = findViewById(R.id.id_btn_enviar);
+        btn.setOnClickListener(view -> Toast.makeText(this, "Se envió sus datos", Toast.LENGTH_SHORT).show());
     }
 }
 
